88dfd04733f1f7fee305c8c46e8e4c0c9e88c850
Abhi <sr7324625@gmail.com>
2023-04-03 23:52:28 +0530

    added news portal

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dbf = require("./src/dbfile");
-require('dotenv').config()
+require("dotenv").config();
 const path = require("path");
-const port = process.env.PORT || 3000
+const NewsAPI = require("newsapi");
+// const fetch = require('node-fetch')
+// import fetch from "node-fetch";
+// const fetch = require("node-fetch")
+const port = process.env.PORT || 3000;
+
+const newsapi = new NewsAPI("9f133693e41344ef9ea4b3e2eed4df22");
+
+// const newsf = (n)=>{
+let data;
+newsapi.v2
+  .everything({
+    q: "technology",
+    from: "2023-12-03",
+    to: "2023-01-14",
+    language: "en",
+    page: 2,
+  })
+  .then((response) => {
+    data = response.articles;
+  })
+  .catch((e) => {
+    data = e;
+  });
+// }
 
 // p = mongoose.connect("mongodb://localhost:27017/registerpage");
-p = mongoose.connect("mongodb+srv://Abhijeet2109:abhijeet21o9@cluster0.ohwhhle.mongodb.net/?retryWrites=true&w=majority"
+p = mongoose.connect(
+  "mongodb+srv://Abhijeet2109:abhijeet21o9@cluster0.ohwhhle.mongodb.net/?retryWrites=true&w=majority"
 );
 
 app.set("view engine", "hbs");
@@ -17,7 +42,7 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.post("/signup", async (req, res) => {
   var name = req.body.name;
@@ -71,6 +96,14 @@ app.post("/verify", async (req, res) => {
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/newspage", (req, res) => {
+  // console.log(newsf(0));
+  res.render("news", { con: data[0]['title'],con1: data[1]['title'],con2: data[2]['title'],con3: data[3]['title'],con4: data[4]['title'] ,con5: data[5]['title'] ,con6: data[6]['title'] ,con7: data[7]['title'] ,con8: data[8]['title'] ,con9: data[9]['title'] ,con10: data[10]['title'], desc: data[0]['description'],desc1: data[1]['description'],desc2: data[2]['description'],desc3: data[3]['description'],desc4: data[4]['description'] ,desc5: data[5]['description'] ,desc6: data[6]['description'] ,desc7: data[7]['description'] ,desc8: data[8]['description'] ,desc9: data[9]['description'] ,desc10: data[10]['description']
+  , urlg: data[0]['url'],urlg1: data[1]['url'],urlg2: data[2]['url'],urlg3: data[3]['url'],urlg4: data[4]['url'] ,urlg5: data[5]['url'] ,urlg6: data[6]['url'] ,urlg7: data[7]['url'] ,urlg8: data[8]['url'] ,urlg9: data[9]['url'] ,urlg10: data[10]['url']
+  , imgu: data[0]['urlToImage'],imgu1: data[1]['urlToImage'],imgu2: data[2]['urlToImage'],imgu3: data[3]['urlToImage'],imgu4: data[4]['urlToImage'] ,imgu5: data[5]['urlToImage'] ,imgu6: data[6]['urlToImage'] ,imgu7: data[7]['urlToImage'] ,imgu8: data[8]['urlToImage'] ,imgu9: data[9]['urlToImage'] ,imgu10: data[10]['urlToImage']});
+});
+
+// urlToImage
 
 app.get("/home", (req, res) => {
   // res.send("Hello World!");
